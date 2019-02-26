c9ab1297d70a18d3b9d98a4a6be43095333b6abd
kanadara13 <jaehyeok.shin@leisureq.com>
2019-02-27 03:08:31 +0900

    Filter 인터페이스 default 추가

--- src/book/filter/Filter.java
+++ src/book/filter/Filter.java
@@ -5,4 +5,8 @@ import book.Book;
 @FunctionalInterface
 public interface Filter {
     boolean test(Book book);
+
+    default void ignoreThidMethod(){
+        System.out.println("함수형 인터페이스의 정의에서 디폴트 메서드는 상관없다는걸 보여주기 위함");
+    }
 }
